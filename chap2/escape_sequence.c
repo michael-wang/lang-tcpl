3adb68087b1ddc0130f7488b3541a7c4126596c7
michael-wang <michael.wang@hey.com>
2025-08-28 15:45:15 +0800

    Use enum for escape sequences

--- chap2/escape_sequence.c
+++ chap2/escape_sequence.c
@@ -1,35 +1,35 @@
 // Show numeric value of escape sequences (p. 38 of the book).
-
 #include <stdio.h>
 
-
-#define BELL		'\a'
-#define BACKSPACE	'\b'
-#define FORMFEED	'\f'
-#define NEWLINE		'\n'
-#define CARRIAGE_RETURN	'\r'
-#define HORZ_TAB	'\t'
-#define VERT_TAB	'\v'
-#define BACKSLASH	'\\'
-#define QUESTION_MARK	'\?'
-#define SINGLE_QUOTE	'\''
-#define DOUBLE_QUOTE	'\"'
-#define OCT_NUMBER	'\000'
-#define HEX_NUMBER	'\x00'
+enum escapes {
+	ALERT_CHAR	= '\a',
+	BACKSPACE	= '\b',
+	FORMFEED	= '\f',
+	NEWLINE		= '\n',
+	RETURN		= '\r',
+	TAB			= '\t',
+	VTAB		= '\v',
+	BACKSLASH	= '\\',
+	QUESTION	= '\?',
+	S_QUOTE		= '\'',
+	D_QUOTE		= '\"',
+	OCT_NUMBER	= '\000',
+	HEX_NUMBER	= '\x00'
+};
 
 int main() {
 	printf("char\thex\tdescription\n");
-	printf("\\a\t%02X\talert (bell) character\n", BELL);
+	printf("\\a\t%02X\talert (bell) character\n", ALERT_CHAR);
 	printf("\\b\t%02X\tbackspace\n", BACKSPACE);
 	printf("\\f\t%02X\tformfeed\n", FORMFEED);
 	printf("\\n\t%02X\tnewline\n", NEWLINE);
-	printf("\\r\t%02X\tcarriage return\n", CARRIAGE_RETURN);
-	printf("\\t\t%02X\thorizontal tab\n", HORZ_TAB);
-	printf("\\v\t%02X\tvertical tab\n", VERT_TAB);
+	printf("\\r\t%02X\tcarriage return\n", RETURN);
+	printf("\\t\t%02X\thorizontal tab\n", TAB);
+	printf("\\v\t%02X\tvertical tab\n", VTAB);
 	printf("\\\\\t%02X\tbackslash\n", BACKSLASH);
-	printf("\\?\t%02X\tquestion mark\n", QUESTION_MARK);
-	printf("\\\'\t%02X\tsingle quote\n", SINGLE_QUOTE);
-	printf("\\\"\t%02X\tdouble quote\n", DOUBLE_QUOTE);
+	printf("\\?\t%02X\tquestion mark\n", QUESTION);
+	printf("\\\'\t%02X\tsingle quote\n", S_QUOTE);
+	printf("\\\"\t%02X\tdouble quote\n", D_QUOTE);
 	printf("\\00\t%02X\toctal number\n", OCT_NUMBER);
 	printf("\\x00\t%02X\thexadecimal numher\n", HEX_NUMBER);
 	return 0;
