d7ae57e06544ab0c8a38faeff74cb09749037ccb
michael-wang <michael.wang@hey.com>
2025-08-28 16:01:07 +0800

    Data driven escape sequences

--- chap2/escape_sequence.c
+++ chap2/escape_sequence.c
@@ -1,36 +1,74 @@
 // Show numeric value of escape sequences (p. 38 of the book).
 #include <stdio.h>
 
-enum escapes {
-	ALERT_CHAR	= '\a',
-	BACKSPACE	= '\b',
-	FORMFEED	= '\f',
-	NEWLINE		= '\n',
-	RETURN		= '\r',
-	TAB			= '\t',
-	VTAB		= '\v',
-	BACKSLASH	= '\\',
-	QUESTION	= '\?',
-	S_QUOTE		= '\'',
-	D_QUOTE		= '\"',
-	OCT_NUMBER	= '\000',
-	HEX_NUMBER	= '\x00'
+// enum escapes {
+// 	ALERT_CHAR	= '\a',
+// 	BACKSPACE	= '\b',
+// 	FORMFEED	= '\f',
+// 	NEWLINE		= '\n',
+// 	RETURN		= '\r',
+// 	TAB			= '\t',
+// 	VTAB		= '\v',
+// 	BACKSLASH	= '\\',
+// 	QUESTION	= '\?',
+// 	S_QUOTE		= '\'',
+// 	D_QUOTE		= '\"',
+// 	OCT_NUMBER	= '\000',
+// 	HEX_NUMBER	= '\x00'
+// };
+const char *titles[] = {
+	"\\a",
+	"\\b",
+	"\\f",
+	"\\n",
+	"\\r",
+	"\\t",
+	"\\v",
+	"\\\\",
+	"\\?",
+	"\\\'",
+	"\\\"",
+	"\\000",
+	"\\x00"
+};
+
+const char escapes[] = {
+	'\a',
+	'\b',
+	'\f',
+	'\n',
+	'\r',
+	'\t',
+	'\v',
+	'\\',
+	'\?',
+	'\'',
+	'\"',
+	'\000',
+	'\x00'
+};
+
+const char *descriptions[] = {
+	"alert (bell) character",
+	"backspace",
+	"formfeed",
+	"newline",
+	"carriage return",
+	"horizontal tab",
+	"vertical tab",
+	"backslash",
+	"question mark",
+	"single quote",
+	"double quote",
+	"octal number",
+	"hexadecimal numher",
 };
 
 int main() {
-	printf("char\thex\tdescription\n");
-	printf("\\a\t%02X\talert (bell) character\n", ALERT_CHAR);
-	printf("\\b\t%02X\tbackspace\n", BACKSPACE);
-	printf("\\f\t%02X\tformfeed\n", FORMFEED);
-	printf("\\n\t%02X\tnewline\n", NEWLINE);
-	printf("\\r\t%02X\tcarriage return\n", RETURN);
-	printf("\\t\t%02X\thorizontal tab\n", TAB);
-	printf("\\v\t%02X\tvertical tab\n", VTAB);
-	printf("\\\\\t%02X\tbackslash\n", BACKSLASH);
-	printf("\\?\t%02X\tquestion mark\n", QUESTION);
-	printf("\\\'\t%02X\tsingle quote\n", S_QUOTE);
-	printf("\\\"\t%02X\tdouble quote\n", D_QUOTE);
-	printf("\\00\t%02X\toctal number\n", OCT_NUMBER);
-	printf("\\x00\t%02X\thexadecimal numher\n", HEX_NUMBER);
+	int i;
+
+	for (i = 0; i < 13; ++i) {
+		printf("%s\t%02X\t%s\n", titles[i], escapes[i], descriptions[i]);
+	}
 	return 0;
 }
